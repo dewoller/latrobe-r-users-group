9b5389f769cc6f3f20ae0ec265dc36402b4eacc9
Dennis Wollersheim <dewoller@gmail.com>
2020-07-16 11:53:17 +1000

    week2

--- R/get_pbs_data.R
+++ R/get_pbs_data.R
@@ -1,6 +1,6 @@
 
 # -------------------------------------------------
-get_pbs_data <- function( database = 'pbs_sample' ) {
+get_pbs_data <- function( chronic_disease = 'PAI', database = 'pbs_sample' ) {
 
   # loads the PostgreSQL driver
   drv <- dbDriver("PostgreSQL")
@@ -8,16 +8,16 @@ get_pbs_data <- function( database = 'pbs_sample' ) {
                    host = "himsql7.latrobe.edu.au", port = 5432,
                    user = "dewollershei", password = Sys.getenv('PBS_LATROBE_PASSWORD' ))
   on.exit(dbDisconnect(con))
-  dbGetQuery( con, "
+  query =  paste0( "
 
-             select supply_date, sex, yob, atc_meaning
-             from pbs  p
-             JOIN patient USING (pin)
-             JOIN pbs_item pi USING (pbs_item)
-             JOIN pbs_atc using( atc_code)
-             where chronic_disease_id='PAI'
-
-             " ) %>% as_tibble()
+                 select supply_date, sex, yob, atc_meaning
+                 from pbs  p
+                 JOIN patient USING (pin)
+                 JOIN pbs_item pi USING (pbs_item)
+                 JOIN pbs_atc using( atc_code)
+                 where chronic_disease_id='", chronic_disease, "'" )
+  print(query)
+  dbGetQuery( con, query) %>% as_tibble()
 
 }
 
--- R/plan.R
+++ R/plan.R
@@ -2,12 +2,12 @@ the_plan <-
   drake_plan(
 
    ## Plan targets in here.
-             df_pbs = get_pbs_data(),
-             df_pbs_large = get_pbs_data('pbs'),
+             df_pbs = get_pbs_data(chronic_disease='PAI', database='pbs_sample'),
+             df_pbs_large = get_pbs_data(chronic_disease='PAI', database='pbs'),
 
              week2 = target(
                                   command = {
-                                    rmarkdown::render(knitr_in("analysis/week2.Rmd"))
+                                    workflowr::wflow_build(knitr_in("analysis/week2.Rmd"))
                                     file_out("docs/week2.html")
                                   }
              ),
